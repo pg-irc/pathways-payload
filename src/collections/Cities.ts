9be2136c9458fbcdecfcddf086dde4b2ef170e2d
rasmus@peacegeeks.org <rasmus@peacegeeks.org>
2022-12-27 09:29:55 -0800

    Issue #1 Add CCC data fields to the city collection

--- src/collections/Cities.ts
+++ src/collections/Cities.ts
@@ -25,6 +25,42 @@ const Cities: CollectionConfig = {
       hasMany: false,
       label: 'Province',
       required: true,
+    },
+    {
+      name: 'ccc-data',
+      type: 'group',
+      fields: [
+        {
+          name: 'climate',
+          // add a link to a particlular hard-coded document for climate?
+          type: 'group',
+          fields: [
+            {
+              name: 'summer-high',
+              type: 'number',
+              // add a link to a particular hard-coded document for summer high?
+            },
+            {
+              name: 'summer-low',
+              type: 'number',
+            }
+          ]
+        },
+        {
+          name: 'people',
+          type: 'group',
+          fields: [
+            {
+              name: 'population',
+              type: 'number',
+            },
+            {
+              name: 'english-speakers-percent',
+              type: 'number',
+            }
+          ]
+        }
+      ]
     }
   ],
 }
