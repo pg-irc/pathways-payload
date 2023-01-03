fd40210f2816b644892c52cef6068fe417260982
rasmus@peacegeeks.org <rasmus@peacegeeks.org>
2023-01-03 15:19:43 -0800

    Issue #1 Refactor

--- src/collections/Cities.ts
+++ src/collections/Cities.ts
@@ -1,30 +1,5 @@
 import { CollectionConfig, FieldHook, Field } from 'payload/types';
-import { CccDatasetBuilder } from '../helpers/ccc_dataset_builder';
-
-const allTheFields = new CccDatasetBuilder()
-    .addDataSet('Climate')
-    .withNumericField('Average days of rain per year')
-    .withNumericField('Summer high', 'centigrade')
-    .withNumericField('Summer low', 'centigrade')
-    .withNumericField('Winter high', 'centigrade')
-    .withNumericField('Winter low', 'centigrade')
-    .addDataSet('People')
-    .withNumericField('City population')
-    .withNumericField('Province population')
-    .withNumericField('English speakers', 'percent')
-    .withNumericField('French speakers', 'percent')
-    .withNumericField('Other speakers', 'percent')
-    .addDataSet('Getting around')
-    .withNumericField('Transit score®')
-    .withNumericField('Walk score®')
-    .addDataSet('Housing')
-    .withNumericField('One bedroom rent average', 'dollars')
-    .withNumericField('Apartment vacancy rate', 'percent')
-    .withNumericField('Two bedroom rent average', 'dollars')
-    .addDataSet('Health info for Ukrainians')
-    .withTextField('Health card')
-    .withTextField('Health and medications')
-    .buildAllDataSets();
+import { allTheFields } from './ccc_fields';
 
 const Cities: CollectionConfig = {
     slug: 'cities',
