9aa8a342eda685577d59412076f8875907d1f6c9
rasmus@peacegeeks.org <rasmus@peacegeeks.org>
2023-01-12 09:12:49 -0800

    progress

--- src/helpers/__tests__/extract_pot.test.ts
+++ src/helpers/__tests__/extract_pot.test.ts
@@ -1,5 +1,22 @@
-describe('foo', () => {
-    it('bar', () => {
-        expect(1).toEqual(2);
+
+const findLocalizedFields = (configuration) => {
+    return [];
+}
+
+describe('extract POT data', () => {
+    describe('find localized fields', () => {
+        it('returns nothing for a collection with no localized fields', () => {
+            const configuration = {
+                slug: 'foo',
+                fields: [
+                    {
+                        name: 'bar',
+                        type: 'number',
+                    },
+                ],
+            };
+            const fields = findLocalizedFields(configuration);
+            expect(fields).toEqual([]);
+        });
     });
 });
