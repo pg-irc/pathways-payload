a78a7906d47dff911f2d61e1d9b631cb1f01bdc3
rasmus@peacegeeks.org <rasmus@peacegeeks.org>
2023-01-12 13:08:24 -0800

    progress

--- src/helpers/__tests__/extract_pot.test.ts
+++ src/helpers/__tests__/extract_pot.test.ts
@@ -5,20 +5,26 @@ import { CollectionConfig, Field, TextField, ArrayField } from 'payload/types';
 import * as R from 'ramda';
 
 const findLocalizedFields = (configuration: CollectionConfig) =>
-    findRecursively(configuration.fields);
+    findRecursively([], configuration.fields);
 
-const findRecursively = (fields: Field[]): string[] => {
+const findRecursively = (path: string[], fields: Field[]): string[][] => {
     const isLocalizedText = (field: Field) =>
         field.type === 'text' && field.localized;
     const localizedFields = R.filter(isLocalizedText, fields);
     let results = [];
     localizedFields.forEach((field: TextField) => {
-        results = [...results, field.name];
+        results = [...results, [...path, field.name]];
     });
-    const isArray = (field: Field): boolean => field.type === 'array';
-    const arrayFields = R.filter(isArray, fields);
-    arrayFields.forEach((field: ArrayField) => {
-        const found = findRecursively(field.fields);
+
+    interface HasFields {
+        name: string;
+        fields: Field[];
+    }
+
+    const hasFields = (field: Field): boolean => R.has('fields', field);
+    const nestedFields: HasFields[] = R.filter(hasFields, fields);
+    nestedFields.forEach((nestedField) => {
+        const found = findRecursively([...path, nestedField.name], nestedField.fields);
         results = [...results, ...found];
     });
     return results;
@@ -60,7 +66,7 @@ describe('extract POT data', () => {
                 ],
             };
             const fields = findLocalizedFields(configuration);
-            expect(fields).toEqual(['bar']);
+            expect(fields).toEqual([['bar']]);
         });
         it('returns names of multiple localized string fields', () => {
             const configuration: CollectionConfig = {
@@ -83,7 +89,7 @@ describe('extract POT data', () => {
                 ],
             };
             const fields = findLocalizedFields(configuration);
-            expect(fields).toEqual(['bar', 'baz']);
+            expect(fields).toEqual([['bar'], ['baz']]);
         });
         describe('configuration with nested fields', () => {
             it('extracts name of localized field in array', () => {
@@ -104,7 +110,27 @@ describe('extract POT data', () => {
                     ],
                 };
                 const fields = findLocalizedFields(configuration);
-                expect(fields).toEqual(['baz']);
+                expect(fields).toEqual([['bar', 'baz']]);
+            });
+            it('extracts name of field inside a group', () => {
+                const configuration: CollectionConfig = {
+                    slug: 'foo',
+                    fields: [
+                        {
+                            name: 'bar',
+                            type: 'group',
+                            fields: [
+                                {
+                                    name: 'baz',
+                                    type: 'text',
+                                    localized: true,
+                                },
+                            ],
+                        },
+                    ],
+                };
+                const fields = findLocalizedFields(configuration);
+                expect(fields).toEqual([['bar', 'baz']]);
             });
         });
     });
