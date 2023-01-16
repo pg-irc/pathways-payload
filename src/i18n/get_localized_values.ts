24badf27b5fc63a69312e91a1d167304955e4ec9
rasmus@peacegeeks.org <rasmus@peacegeeks.org>
2023-01-16 08:52:03 -0800

    CleanUp

--- src/i18n/get_localized_values.ts
+++ src/i18n/get_localized_values.ts
@@ -27,27 +27,42 @@ const getLocalizedValuesRecursively = (
         return [];
     }
     if (R.is(Array, object[path[0]])) {
-        let result = [];
-        let index = 0;
-        object[path[0]].forEach((element: any) => {
-            const p = R.drop(1, path);
-            const b = appendToBreadCrumbs(breadCrumbs, path[0], index);
-            const r = getLocalizedValuesRecursively(p, b, element);
-            index += 1;
-            result = [...result, r];
-        });
-        return result;
+        return getLocalizedValuesFromArray(path, breadCrumbs, object);
     }
     if (path.length === 1) {
-        const v = R.is(String, object[path[0]]) ? object[path[0]] : undefined;
-        if (!v) {
-            return undefined;
-        }
-        const b = appendToBreadCrumbs(breadCrumbs, path[0]);
-        return [{ value: v, breadCrumbs: b }];
+        return getLocalizedValueFromLeafNode(path, breadCrumbs, object);
     }
     const b = appendToBreadCrumbs(breadCrumbs, path[0]);
     return getLocalizedValuesRecursively(R.drop(1, path), b, object[path[0]]);
+};
+
+const getLocalizedValuesFromArray = (
+    path: string[],
+    breadCrumbs: string,
+    object: Object
+) => {
+    let result = [];
+    let index = 0;
+    object[path[0]].forEach((element: any) => {
+        const p = R.drop(1, path);
+        const b = appendToBreadCrumbs(breadCrumbs, path[0], index);
+        result = [...result, getLocalizedValuesRecursively(p, b, element)];
+        index += 1;
+    });
+    return result;
+};
+
+const getLocalizedValueFromLeafNode = (
+    path: string[],
+    breadCrumbs,
+    object: Object
+) => {
+    const isString = R.is(String, object[path[0]]);
+    if (!isString) {
+        return undefined;
+    }
+    const b = appendToBreadCrumbs(breadCrumbs, path[0]);
+    return [{ value: object[path[0]], breadCrumbs: b }];
 };
 
 const appendToBreadCrumbs = (
@@ -57,8 +72,8 @@ const appendToBreadCrumbs = (
 ): string => {
     const isString = R.is(String, item);
     const result = isString
-        ? breadcrumbs + '.' + item
-        : breadcrumbs + '[' + String(item) + ']';
+        ? `${breadcrumbs}.${item}`
+        : `${breadcrumbs}[${item}]`;
     if (secondItem === undefined) {
         return result;
     }
